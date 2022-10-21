56f1e7e75a71ff445324012de48059e45faa9f77
Edward Rossiano <edwardrossiano@gmail.com>
2022-10-21 13:20:06 -0500

    Aplicacion REACT demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Movie from './components/Movie';
+
+const API_KEY = 'b4717d9d6dc5c95dc09a72ee06e90e91';
+
+const FEATURED_API = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`;
+
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&page=1&include_adult=false&query=`;
 
 function App() {
+  
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(
+   () => {
+      getMovies(FEATURED_API);
+    }
+    ,[]
+  );
+  
+
+  const getMovies = (API) => {
+    fetch(API)
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data);
+      setMovies(data.results);
+    });
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm){
+        getMovies(SEARCH_API + searchTerm);
+    };
+    setSearchTerm('');
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <form onSubmit={handleOnSubmit}>
+            <input className="search"
+              type="search"
+              placeholder="Search..."
+              value={searchTerm}
+              onChange={handleOnChange}
+            />
+        </form>
+        
       </header>
-    </div>
+      <div className='movie-container'>
+        {movies.map( movie => (
+          <Movie key={movie.id} {...movie} />
+        ))}
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+/**
+ * DISCOVER
+ * https://api.themoviedb.org/3/discover/movie?api_key=<<api_key>>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate
+ * 
+ * TOP RATED
+ * https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1
+ * 
+ * 
+ * SEARCH MOVIE
+ * https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&language=en-US&page=1&include_adult=false
+ * 
+ * IMAGES
+ * https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
+ * 
+ * 
+ */
---
+++ src/components/Movie.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const IMG_API = `https://image.tmdb.org/t/p/w1280`;
+
+const setVoteClass = (vote) => {
+    if(vote >= 8 ){
+        return 'green';
+    }else if(vote >= 6){
+        return 'orange';
+    }else{
+        return 'red';
+    }
+}
+
+const Movie = ({title, poster_path, overview, vote_average}) => (
+    <div className="movie">
+       
+        <img src={(poster_path? IMG_API+poster_path : 'https://img.freepik.com/vector-gratis/fondo-pelicula-casera-palomitas-maiz_1419-1852.jpg?w=350')} alt={title} />
+        <div className="movie-info">
+            <h3>{title}</h3>
+            <span className={`tag ${setVoteClass(vote_average)} `}>{vote_average}</span>
+        </div>
+        <div className="movie-over">
+            <h2>Overview: </h2>
+            <p>{overview}</p>
+            </div> 
+
+
+    </div>
+
+);
+
+export default Movie;
